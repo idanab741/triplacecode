97f0eb5b42dbfe64edeb00e86aafbdd4b623a18c
idanab741 <idanab741@gmail.com>
2026-07-16 01:33:47 +0300

    Update DiscoverCard.tsx

--- src/screens/home/DiscoverCard.tsx
+++ src/screens/home/DiscoverCard.tsx
@@ -1,11 +1,29 @@
 export function DiscoverCard() {
   return (
-    <div className="px-6">
-      <img
-        src="/images/discover/tripmatch.png"
-        alt=""
-        style={{ width: "100%", display: "block" }}
-      />
+    <div style={{ padding: 24 }}>
+      <div
+        style={{
+          display: "flex",
+          overflowX: "auto",
+          gap: 20,
+          border: "2px solid red",
+        }}
+      >
+        <img
+          src="/images/discover/tripmatch.png"
+          style={{ width: 300, flex: "0 0 auto" }}
+        />
+
+        <img
+          src="/images/discover/ai-powered.png"
+          style={{ width: 300, flex: "0 0 auto" }}
+        />
+
+        <img
+          src="/images/discover/places.png"
+          style={{ width: 300, flex: "0 0 auto" }}
+        />
+      </div>
     </div>
   );
 }
