fde0b2ffeeaea5bedb9ccdd2a28e967cfb19d869
idanab741 <idanab741@gmail.com>
2026-09-19 02:49:08 +0300

    remove stray src-fixed-check folder

--- src-fixed-check/src/app/onboarding/tripmatch/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { FeatureOnboardingScreen } from "@/screens/onboarding/FeatureOnboardingScreen";
-import { Slide3TripMatch } from "@/screens/onboarding/slides/Slide3TripMatch";
-
-export default function TripMatchOnboardingPage() {
-  return <FeatureOnboardingScreen feature="tripmatch" destination="/tripmatch" Slide={Slide3TripMatch} />;
-}
